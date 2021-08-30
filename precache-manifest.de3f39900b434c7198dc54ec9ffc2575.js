4dc36bd2ea79fefe4b80e189a3bb0b1f6f0c22ed
ingvarrfx <nurov.mj@gmail.com>
2021-08-30 12:57:06 +0300

    Updates

---
+++ precache-manifest.de3f39900b434c7198dc54ec9ffc2575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95295895d5c1594bb1c35f69aee53d1",
+    "url": "/test_todolist/index.html"
+  },
+  {
+    "revision": "89374c80b1fedd9e70de",
+    "url": "/test_todolist/static/css/main.2a7eaf4e.chunk.css"
+  },
+  {
+    "revision": "76f8d08f09736743dc24",
+    "url": "/test_todolist/static/js/2.450f50a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test_todolist/static/js/2.450f50a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89374c80b1fedd9e70de",
+    "url": "/test_todolist/static/js/main.8aaf29e3.chunk.js"
+  },
+  {
+    "revision": "a1815fd68b1706a77b72",
+    "url": "/test_todolist/static/js/runtime-main.16314e80.js"
+  }
+]);
